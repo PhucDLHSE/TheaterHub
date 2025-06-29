c5ff2a5becf5b9e71e5c0abf5fbf4bde5e1d3b9f
PhucDLHSE <danglehoangphuc2003@gmail.com>
2025-06-29 23:26:48 +0700

    change firebase-adminsdk.json -> .env

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,10 +1,10 @@
 const admin = require("firebase-admin");
-const path = require("path");
-const serviceAccount = require("./firebase-adminsdk.json"); 
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: "theaterhub-storage.firebasestorage.app", 
+  storageBucket: "theaterhub-storage.appspot.com", 
 });
 
 const bucket = admin.storage().bucket();
