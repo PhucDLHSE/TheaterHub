f45528c03a7ee9bff9b0a4a7425bbdeba95cb5aa
PhucDLHSE <danglehoangphuc2003@gmail.com>
2025-06-29 23:32:57 +0700

    encoded firebase_adminsdk.sjon-> base64

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,14 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+const base64Config = process.env.FIREBASE_CONFIG_BASE64;
+if (!base64Config) {
+  console.error("Missing FIREBASE_CONFIG_BASE64");
+  process.exit(1);
+}
+
+const serviceAccount = JSON.parse(
+  Buffer.from(base64Config, "base64").toString("utf8")
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
